5ed2d8a95ff34a4cc8f55ce88c8c3a665f428679
yobo <33770985+yhz542@users.noreply.github.com>
2018-09-23 15:34:50 +0800

    Create AVLTree.c

---
+++ AVLTree.c
@@ -0,0 +1,139 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct TreeNode *AVLTree ;
+typedef AVLTree Position ;
+typedef int ElementType ;
+struct TreeNode
+{
+        ElementType Data ;
+        AVLTree Left ;//左子树
+        AVLTree Right ;//右子树
+        int Height ;//树高
+};
+void PreOrderTraversal( AVLTree );//先序遍历
+void InOrderTraversal( AVLTree );//中序遍历
+void PostOrderTraversal( AVLTree );//后序遍历
+Position Find( ElementType X , AVLTree BST );//查找
+//平衡二叉树自适应插入
+int Max( int , int ) ;
+int GetHeight( AVLTree );
+AVLTree SingleLeftRotation( AVLTree );
+AVLTree SingleRightRotation( AVLTree) ;
+AVLTree DoubleLeftRightRotation( AVLTree );
+AVLTree DoubleRightLeftRotation( AVLTree );
+AVLTree Insert( AVLTree , ElementType X ) ;
+//平衡二叉树自适应插入
+int main(void)
+{
+        return 0 ;
+}
+void PreOrderTraversal( AVLTree BT )
+{
+        if( BT )
+        {
+                printf( "%d" , BT->Data ) ;
+                PreOrderTraversal( BT->Left ) ;
+                PreOrderTraversal( BT->Right ) ;
+        }
+}
+void InOrderTraversal( AVLTree BT )
+{
+        if( BT )
+        {
+                InOrderTraversal( BT->Left ) ;
+                printf( "%d" , BT->Data ) ;
+                InOrderTraversal( BT->Right ) ;
+        }
+}
+void PostOrderTraversal( AVLTree BT )
+{
+        if( BT )
+        {
+                PostOrderTraversal( BT->Left ) ;
+                PostOrderTraversal( BT->Right ) ;
+                printf( "%d" , BT->Data ) ;
+        }
+}
+Position Find( ElementType X , AVLTree BST )
+{
+        if( !BST )
+                return NULL;
+        if( X > BST->Data )
+                return Find( X , BST->Right ) ;
+        else if( X < BST->Data)
+                return Find( X , BST->Left ) ;
+        else
+                return BST;//查找成功
+}
+int GetHeight( AVLTree A )
+{
+        int Height ;
+        if( !A )
+                Height = -1 ;
+        else
+                Height = A->Height ;
+        return Height ;
+}
+int Max( int a , int b )
+{
+        return a > b ? a : b ;
+}
+AVLTree SingleLeftRotation( AVLTree A )
+{
+        AVLTree B = A->Left ;
+        A->Left = B->Right ;
+        B->Right = A ;
+        A->Height = Max( GetHeight(A->Left) , GetHeight(A->Right))+1;
+        B->Height = Max( GetHeight(B->Left) , A->Height)+1;
+        return B;
+}
+AVLTree SingleRightRotation( AVLTree A )
+{
+        AVLTree B = A->Right ;
+        A->Right = B->Left ;
+        B->Left = A ;
+        A->Height = Max( GetHeight(A->Left) , GetHeight(A->Right))+1;
+        B->Height = Max( GetHeight(B->Right) , A->Height )+1;
+        return B ;
+}
+AVLTree DoubleLeftRightRotation( AVLTree A )
+{
+        A->Left = SingleRightRotation(A->Left);
+        return SingleLeftRotation(A);
+}
+AVLTree DoubleRightLeftRotation( AVLTree A )
+{
+        A->Right = SingleLeftRotation(A->Right) ;
+        return SingleRightRotation( A ) ;
+}
+AVLTree Insert( AVLTree T , ElementType X )
+{
+        if(!T)//如果当前为空树，创建并插入子节点
+        {
+                T=(AVLTree)malloc(sizeof( struct TreeNode ) );
+                T->Data = X;
+                T->Height = 0 ;
+                T->Left = T->Right = NULL;
+        }
+        else if( X < T->Data )//插入左边
+        {
+                T->Left = Insert( T->Left , X );
+                if( GetHeight(T->Left) - GetHeight( T->Right ) == 2 )
+                        if( X < T->Left->Data )
+                                T = SingleLeftRotation( T );
+                        else
+                                T = DoubleLeftRightRotation( T ) ;
+        }
+        else if( X > T->Data )//插入右边
+        {
+                T->Right = Insert( T->Right , X ) ;
+                if( GetHeight( T->Left ) - GetHeight( T->Right ) == -2 )
+                        if( X > T->Right->Data )
+                                T = SingleRightRotation( T ) ;
+                        else
+                                T = DoubleRightLeftRotation( T ) ;
+        }
+        T->Height = Max( GetHeight( T->Left ) , GetHeight( T->Right ) ) + 1 ;
+        return T ;
+}
+//每次插入点只会将路径上的所有节点高度加一，不在路径上的节点不做任何操作。
