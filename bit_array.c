44cec19d35cbe8993b231731d3ecdde932690284
yobo <33770985+yhz542@users.noreply.github.com>
2021-03-09 00:07:30 +0800

    Create bit_array.c

---
+++ bit_array.c
@@ -0,0 +1,34 @@
+//把指定位设置为1 从0开始
+void set_bit(char bit_array[],unsigned bit_number)
+{
+        unsigned indexOfArray = bit_number / 8 ;
+        unsigned indexOfBit = bit_number % 8;
+        bit_array[indexOfArray] |= (1<<indexOfBit);
+}
+//把指定位清零
+void clear_bit(char bit_array[],unsigned bit_number)
+{
+        unsigned indexOfArray = bit_number / 8 ;
+        unsigned indexOfBit = bit_number % 8;
+        bit_array[indexOfArray] &= ~(1<<indexOfBit);
+}
+//如果value=0,把指定位清零，否则设置为1
+void assign_bit(char bit_array[],unsigned bit_number,int value)
+{
+        if(value)
+        {
+                set_bit(bit_array,bit_number);
+        }
+        else
+        {
+                clear_bit(bit_array,bit_number);
+        }
+}
+int test_bit(char bit_array[],unsigned bit_number)
+{
+        int result = 0;
+        unsigned indexOfArray = bit_number / 8 ;
+        unsigned indexOfBit = bit_number % 8;
+        result = bit_array[indexOfArray] & (1<<indexOfBit);
+        return result;
+}
